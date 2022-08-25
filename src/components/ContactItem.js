8d85eb88e155baa4e9457563fabf1982404386b5
DESKTOP-EM7KJIP\R4HA <erbees.50000@gmail.com>
2022-08-25 18:00:17 +0700

    first commit

---
+++ src/components/ContactItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactItemImage from "./ContactItemImages";
+import ContactItemBody from "./ContactItemBody";
+
+function ContactItem({ imageUrl, name, tag }) {
+  return (
+    <div className="contact-item">
+      <ContactItemImage imageUrl={imageUrl} />
+      <ContactItemBody name={name} tag={tag} />
+    </div>
+  );
+}
+
+export default ContactItem;
